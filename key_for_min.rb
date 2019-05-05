a25e90d990e235c95048782ab5fa83a5642306f0
rudi hinds <rudihinds@gmail.com>
2019-05-05 16:09:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,20 @@
-
 def key_for_min_value(hash)
-new = hash.sort_by do |key, value|
-  value 
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
-new.to_h.keys[0]
-end
+
+
+# def key_for_min_value(hash)
+# new = hash.sort_by do |key, value|
+#   value 
+# end
+# new.to_h.keys[0]
+# end
 
